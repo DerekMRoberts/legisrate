783824c81800033b5309f4fbfdef8ab545f5cd3e
Derek Roberts <derek.roberts@kysu.edu>
2021-01-08 15:09:23 -0500

    boilerplate

---
+++ src/main/java/com/grouptwo/legisrate/LegisrateApplication.java
@@ -0,0 +1,13 @@
+package com.grouptwo.legisrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegisrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegisrateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grouptwo/legisrate/LegisrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grouptwo.legisrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegisrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
