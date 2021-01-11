344b3f9b1f8f409fdd7a24d4b8df583ec131974c
Russell Taylor <rdtaylorjr@protonmail.com>
2021-01-11 11:46:24 -0600

    Added comments

--- src/main/java/com/grouptwo/legisrate/model/Legislation.java
+++ src/main/java/com/grouptwo/legisrate/model/Legislation.java
@@ -1,5 +1,8 @@
 package com.grouptwo.legisrate.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 /**
@@ -17,60 +20,117 @@ import java.util.Objects;
 public class Legislation {
 
     private int legislationID;
+    @NotBlank(message = "Legislation title must not be empty.")
+    @Size(max = 45, message = "Legislation title must be fewer than 45 characters")
     private String title;
-    private String sponsor;  // add?
-    private boolean active;
+    @NotBlank(message = "Legislation summary must not be empty.")
+    @Size(max = 90, message = "Legislation summary must be fewer than 90 characters")
     private String summary;
+    private boolean active;
+    private String sponsor;  // add?
     private String pdfUrl;  // add?
 
+    /**
+     * Gets the legislation ID
+     * @return the legislation ID
+     */
     public int getLegislationID() {
         return legislationID;
     }
 
+    /**
+     * Sets the legislation ID
+     * @param legislationID the legislation ID
+     */
     public void setLegislationID(int legislationID) {
         this.legislationID = legislationID;
     }
 
+    /**
+     * Gets the legislation title
+     * @return the legislation title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the legislation title
+     * @param title the legislation title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getSponsor() {
-        return sponsor;
-    }
-
-    public void setSponsor(String sponsor) {
-        this.sponsor = sponsor;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
+    /**
+     * Gets the legislation summary
+     * @return the legislation summary
+     */
     public String getSummary() {
         return summary;
     }
 
+    /**
+     * Sets the legislation summary
+     * @param summary the legislation summary
+     */
     public void setSummary(String summary) {
         this.summary = summary;
     }
 
+    /**
+     * Gets a boolean indicating whether the legislation is active
+     * @return a boolean indicating whether the legislation is active
+     */
+    public boolean isActive() {
+        return active;
+    }
+
+    /**
+     * Sets a boolean indicating whether the legislation is active
+     * @param active a boolean indicating whether the legislation is active
+     */
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    /**
+     * Gets the legislation sponsor
+     * @return the legislation sponsor
+     */
+    public String getSponsor() {
+        return sponsor;
+    }
+
+    /**
+     * Sets the legislation sponsor
+     * @param sponsor the legislation sponsor
+     */
+    public void setSponsor(String sponsor) {
+        this.sponsor = sponsor;
+    }
+
+    /**
+     * Gets the URL of the legislation pdf
+     * @return the URL of the legislation pdf
+     */
     public String getPdfUrl() {
         return pdfUrl;
     }
 
+    /**
+     * Sets the URL of the legislation pdf
+     * @param pdfUrl the URL of the legislation pdf
+     */
     public void setPdfUrl(String pdfUrl) {
         this.pdfUrl = pdfUrl;
     }
 
+    /**
+     * Determines whether two Legislation objects are equal
+     * @param o the object to be compared with the current object
+     * @return a boolean indicating whether the two objects are equal
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -79,9 +139,28 @@ public class Legislation {
         return legislationID == that.legislationID && active == that.active && Objects.equals(title, that.title) && Objects.equals(sponsor, that.sponsor) && Objects.equals(summary, that.summary) && Objects.equals(pdfUrl, that.pdfUrl);
     }
 
+    /**
+     * Overrides the hashCode() method
+     * @return the object as a hash
+     */
     @Override
     public int hashCode() {
         return Objects.hash(legislationID, title, sponsor, active, summary, pdfUrl);
     }
 
+    /**
+     * Overrides the toString() method
+     * @return the object as a String
+     */
+    @Override
+    public String toString() {
+        return "Legislation{" +
+                "legislationID=" + legislationID +
+                ", title='" + title + '\'' +
+                ", summary='" + summary + '\'' +
+                ", active=" + active +
+                ", sponsor='" + sponsor + '\'' +
+                ", pdfUrl='" + pdfUrl + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/grouptwo/legisrate/model/Review.java
+++ src/main/java/com/grouptwo/legisrate/model/Review.java
@@ -1,5 +1,8 @@
 package com.grouptwo.legisrate.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 /**
@@ -17,51 +20,98 @@ import java.util.Objects;
 public class Review {
 
     private int reviewID;
+    @NotNull(message = "Legislation ID must not be empty.")
     private int legislationID;
+    @NotNull(message = "User ID must not be empty.")
     private int userID;
     private String comments;
     private int rating;
 
+    /**
+     * Gets the review ID
+     * @return the review ID
+     */
     public int getReviewID() {
         return reviewID;
     }
 
+    /**
+     * Sets the review ID
+     * @param reviewID the review ID
+     */
     public void setReviewID(int reviewID) {
         this.reviewID = reviewID;
     }
 
+    /**
+     * Gets the legislation ID
+     * @return the legislation ID
+     */
     public int getLegislationID() {
         return legislationID;
     }
 
+    /**
+     * Sets the legislation ID
+     * @param legislationID the legislation ID
+     */
     public void setLegislationID(int legislationID) {
         this.legislationID = legislationID;
     }
 
+    /**
+     * Gets the user ID
+     * @return the user ID
+     */
     public int getUserID() {
         return userID;
     }
 
+    /**
+     * Sets the user ID
+     * @param userID the user ID
+     */
     public void setUserID(int userID) {
         this.userID = userID;
     }
 
+    /**
+     * Gets the review comments
+     * @return the review comments
+     */
     public String getComments() {
         return comments;
     }
 
+    /**
+     * Sets the review comments
+     * @param comments the review comments
+     */
     public void setComments(String comments) {
         this.comments = comments;
     }
 
+    /**
+     * Gets the review rating
+     * @return the review rating
+     */
     public int getRating() {
         return rating;
     }
 
+    /**
+     * Sets the review rating
+     * @param rating the review rating
+     */
     public void setRating(int rating) {
         this.rating = rating;
     }
 
+    /**
+     * Determines whether two Review objects are equal
+      @param o the object to be compared with the current object
+     * @return a boolean indicating whether the two objects are equal
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -70,9 +120,28 @@ public class Review {
         return reviewID == review.reviewID && legislationID == review.legislationID && userID == review.userID && rating == review.rating && Objects.equals(comments, review.comments);
     }
 
+    /**
+     * Overrides the hashCode() method
+     * @return the object as a hash
+     */
     @Override
     public int hashCode() {
         return Objects.hash(reviewID, legislationID, userID, comments, rating);
+    }
+
+    /**
+     * Overrides the toString() method
+     * @return the object as a String
+     */
+    @Override
+    public String toString() {
+        return "Review{" +
+                "reviewID=" + reviewID +
+                ", legislationID=" + legislationID +
+                ", userID=" + userID +
+                ", comments='" + comments + '\'' +
+                ", rating=" + rating +
+                '}';
     }
 
 }
