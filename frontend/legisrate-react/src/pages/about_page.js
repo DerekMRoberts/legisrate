e71426180b081dba73137e6e4b9306ae155f246b
rosebudmaria <73312741+rosebudmaria@users.noreply.github.com>
2021-01-13 13:38:07 -0500

    commit to Rosalinda

--- frontend/legisrate-react/src/pages/about_page.js
+++ frontend/legisrate-react/src/pages/about_page.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 function AboutPage() {
-    const developerName = 'Rosalinda P'
-    const developerName2 = 'Derek R'
+    const developerName = 'Rosalinda P.'
+    const developerName2 = 'Derek R.'
+    const developerName3 = 'John Michael R.'
+    const developerName4 = 'Abdulrasaq S'
+    const developerName5 = 'Russell T.'
     return (
         <div id="about_page" className="App-page">
             <h1>Our Project</h1>
@@ -12,6 +15,18 @@ function AboutPage() {
             <h2>Team</h2>
             <p>
                 Contributing Developers: {developerName}
+            </p>
+            <p>
+                {developerName2}
+            </p>
+            <p>
+                {developerName3}
+            </p>
+            <p>
+                {developerName4}
+            </p>
+            <p>
+                {developerName5}
             </p>
         </div>
     )
