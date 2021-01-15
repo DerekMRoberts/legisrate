d906dde454e29180f9035ed13d528864cc2c1078
Derek Roberts <derek.roberts@kysu.edu>
2021-01-15 09:50:45 -0500

    about page fix

--- frontend/legisrate-react/src/pages/about_page.js
+++ frontend/legisrate-react/src/pages/about_page.js
@@ -8,25 +8,35 @@ function AboutPage() {
     const developerName5 = 'Russell T.'
     return (
         <div id="about_page" className="App-page">
-            <h1>Our Project</h1>
-            <p>
-                We're creating a simple React application.
-            </p>
+            <h1>LegisRate Project</h1>
+            <div className="container-sm">
+                <p>
+                    LegisRate is a web application built to elicit engagement
+                    from the population in the legislative process in a way
+                    that is direct, straight-forward, and intuitive. Users will
+                    be able to search for legislation and review it and give it
+                    an overall rating based on a 5 point rating scale.
+                </p>
+            </div>
             <h2>Team</h2>
             <p>
-                Contributing Developers: {developerName}
+                Contributing Developers:
             </p>
             <p>
-                {developerName2}
+                {developerName2} - Team Lead, Front End
+
             </p>
             <p>
-                {developerName3}
+                {developerName} - Front End
             </p>
             <p>
-                {developerName4}
+                {developerName3} - Database
             </p>
             <p>
-                {developerName5}
+                {developerName4} - Database
+            </p>
+            <p>
+                {developerName5} - Back End
             </p>
         </div>
     )
